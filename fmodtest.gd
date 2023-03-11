0ca6dd6a79d0aa27c9fec0c533748b5efa1feaf2
IlmariHirvonen <ilmarinukkeli@gmail.com>
2023-03-11 15:49:19 +0200

    Initial commit

---
+++ fmodtest.gd
@@ -0,0 +1,19 @@
+extends Node
+
+func _ready():
+	# set up FMOD
+	Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_STEREO, 0)
+	Fmod.init(1024, Fmod.FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.FMOD_INIT_NORMAL)
+	
+	# load banks
+	Fmod.load_bank("res://Banks/Desktop/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://Banks/Desktop/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	
+	#godot_fmod.playOneShot("event:/Level 01", self)
+	
+	# register listener
+	#godot_fmod.addListener(self.get_viewport())
+	Fmod.add_listener(0, self)
+	
+	# play some events
+	Fmod.play_one_shot("event:/test", self)
